39cbc3148ed54953d4f6ed6fa4da136d7cdb4c48
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2023-04-12 14:22:16 -0300

    exercicios

---
+++ revisao/09- condicionais/main.js
@@ -0,0 +1,19 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Math.floor(10)
+
+
+
+// condicionais parte II
+
+
+
+const pet = 'cachorro';
+if (pet === 'cachorro') {
+  console.log('au au');
+} else if (pet === 'gato') {
+  console.log('miau');
+} else if (pet === 'vaca') {
+  console.log('Muuu');
+} else {
+  console.log('sem barulho conhecido');
+}
---
+++ revisao/10- lacos/main.js
@@ -0,0 +1,7 @@
+const person = {
+  name: 'Mateus Carvalho',
+  age: 26,
+  devFront: true,
+  devBack: false,
+  favoriteGames: ['Chrono Trigger',]
+}
